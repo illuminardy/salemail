4409b582b7ef3cce70e6be71ec218179508dc8ad
Yanardy Sanchez <yanardy.sanchez@jet.com>
2020-04-10 22:35:31 -0400

    git ignored capitalization of fodler name causing Heroku to fail

---
+++ src/components/Header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './header.scss';
+
+const Header = (props) => {
+  return(
+    <div className="header-container">
+      <div className="header-logo">
+        Sale Mail
+      </div>
+    </div>
+  )
+};
+
+export default Header;
